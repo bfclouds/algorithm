d6425785963b41ef4e5c9eef6528954fac699aaf
bfclouds <834298507@qq.com>
2020-03-23 14:50:38 +0800

    fix check perfect number

--- checkPerfectNumber.js
+++ checkPerfectNumber.js
@@ -4,15 +4,19 @@
 // 给定一个 整数 n， 如果他是完美数，返回 True，否则返回 False
 
 const checkPerfectNumber = function (nums) {
-    if (nums === 1) {
+    if (nums === 1 || nums === 0) {
         return false
     }
-    let value = 0;
-    for (let i = 1; i < nums; i++) {
+    let value = 1;
+    let sqrt = Math.ceil(Math.sqrt(nums));
+
+    for (let i = 2; i < sqrt; i++) {
         if (nums % i === 0) {
             value += i;
+            value += nums / i;
         }
     }
+
     return value === nums;
 };
 
